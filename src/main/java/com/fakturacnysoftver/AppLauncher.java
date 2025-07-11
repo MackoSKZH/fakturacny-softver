2a10abcb375a0592936bfb4d6f8c41bc4f631fc4
Martin <martin.hronsky.007@gmail.com>
2025-07-11 17:07:31 +0200

    Changing headline of GUI

--- src/main/java/com/fakturacnysoftver/AppLauncher.java
+++ src/main/java/com/fakturacnysoftver/AppLauncher.java
@@ -11,7 +11,7 @@ public class AppLauncher extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/MainView.fxml"));
         Scene scene = new Scene(loader.load());
-        primaryStage.setTitle("Fakturačný softvér – Farnosť SPM");
+        primaryStage.setTitle("Fakturačný softvér");
         primaryStage.setScene(scene);
         primaryStage.setMinWidth(820);
         primaryStage.setMinHeight(720);
