2ca5bf9f8230277c41e68cfd8a684fc7b01488a8
Martin <martin.hronsky.007@gmail.com>
2025-07-11 17:17:19 +0200

    Adding new data of receiver

--- src/main/java/com/fakturacnysoftver/FakturaData.java
+++ src/main/java/com/fakturacnysoftver/FakturaData.java
@@ -8,9 +8,20 @@ import java.util.List;
 public class FakturaData {
     private String odberatelMeno;
     private String odberatelAdresa;
+    private String odberatelIco;
+    private String odberatelDic;
+    private String odberatelIcdph;
+    private String odberatelTelefon;
+    private String odberatelEmail;
+    private String bankaNazov;
+    private String bankaIban;
+    private String bankaSwift;
+    private String variabilnySymbol;
+
     private LocalDate datum;
     private List<Item> polozky;
 
+    // KONŠTRUKTOR (zatiaľ základný, môžeš rozšíriť podľa potreby)
     public FakturaData(String odberatelMeno, String odberatelAdresa, LocalDate datum, List<Item> polozky) {
         this.odberatelMeno = odberatelMeno;
         this.odberatelAdresa = odberatelAdresa;
@@ -18,8 +29,110 @@ public class FakturaData {
         this.polozky = polozky;
     }
 
+    // GETTRE a SETTRE
+
+    public String getOdberatelMeno() {
+        return this.odberatelMeno;
+    }
+
+    public void setOdberatelMeno(String odberatelMeno) {
+        this.odberatelMeno = odberatelMeno;
+    }
+
+    public String getOdberatelAdresa() {
+        return this.odberatelAdresa;
+    }
+
+    public void setOdberatelAdresa(String odberatelAdresa) {
+        this.odberatelAdresa = odberatelAdresa;
+    }
+
+    public String getOdberatelIco() {
+        return this.odberatelIco;
+    }
+
+    public void setOdberatelIco(String odberatelIco) {
+        this.odberatelIco = odberatelIco;
+    }
+
+    public String getOdberatelDic() {
+        return this.odberatelDic;
+    }
+
+    public void setOdberatelDic(String odberatelDic) {
+        this.odberatelDic = odberatelDic;
+    }
+
+    public String getOdberatelIcdph() {
+        return this.odberatelIcdph;
+    }
+
+    public void setOdberatelIcdph(String odberatelIcdph) {
+        this.odberatelIcdph = odberatelIcdph;
+    }
+
+    public String getOdberatelTelefon() {
+        return this.odberatelTelefon;
+    }
+
+    public void setOdberatelTelefon(String odberatelTelefon) {
+        this.odberatelTelefon = odberatelTelefon;
+    }
+
+    public String getOdberatelEmail() {
+        return this.odberatelEmail;
+    }
+
+    public void setOdberatelEmail(String odberatelEmail) {
+        this.odberatelEmail = odberatelEmail;
+    }
+
+    public String getBankaNazov() {
+        return this.bankaNazov;
+    }
+
+    public void setBankaNazov(String bankaNazov) {
+        this.bankaNazov = bankaNazov;
+    }
+
+    public String getBankaIban() {
+        return this.bankaIban;
+    }
+
+    public void setBankaIban(String bankaIban) {
+        this.bankaIban = bankaIban;
+    }
+
+    public String getBankaSwift() {
+        return this.bankaSwift;
+    }
+
+    public void setBankaSwift(String bankaSwift) {
+        this.bankaSwift = bankaSwift;
+    }
+
+    public String getVariabilnySymbol() {
+        return this.variabilnySymbol;
+    }
+
+    public void setVariabilnySymbol(String variabilnySymbol) {
+        this.variabilnySymbol = variabilnySymbol;
+    }
+
+    public LocalDate getDatum() {
+        return this.datum;
+    }
+
+    public void setDatum(LocalDate datum) {
+        this.datum = datum;
+    }
+
     public List<Item> getPolozky() {
         return this.polozky;
+    }
+
+    public void setPolozky(List<Item> polozky) {
+        this.polozky = polozky;
     }
 
     public double getSubtotal() {
@@ -34,19 +147,7 @@ public class FakturaData {
         return this.polozky.stream().mapToDouble(Item::getCenaSDph).sum();
     }
 
-    public String getOdberatelMeno() {
-        return this.odberatelMeno;
-    }
-
-    public String getOdberatelAdresa() {
-        return this.odberatelAdresa;
-    }
-
-    public LocalDate getDatum() {
-        return this.datum;
-    }
-
-
+    // VNORENÁ TRIEDA Item
     public static class Item {
         private final StringProperty popis = new SimpleStringProperty("");
         private final IntegerProperty mnozstvo = new SimpleIntegerProperty(1);
