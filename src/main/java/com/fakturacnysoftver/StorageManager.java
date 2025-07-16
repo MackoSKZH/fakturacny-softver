c82a5208b7796237a4d5422ebf1ca13fe078ae39
Martin <martin.hronsky.007@gmail.com>
2025-07-16 17:57:06 +0200

    Renaming variables and methods for user

--- src/main/java/com/fakturacnysoftver/StorageManager.java
+++ src/main/java/com/fakturacnysoftver/StorageManager.java
@@ -1,26 +1,28 @@
 package com.fakturacnysoftver;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class StorageManager {
-    private static final Path BIN_PATH = Path.of(System.getProperty("user.home"), ".farnost", "farnost.bin");
+    private static final Path BIN_PATH = Path.of(System.getProperty("user.home"), ".user", "user.bin");
 
-    public static FarnostData loadFarnost() {
+    public static UserData loadUser() {
         try {
             if (Files.exists(BIN_PATH)) {
                 try (ObjectInputStream in = new ObjectInputStream(Files.newInputStream(BIN_PATH))) {
-                    return (FarnostData)in.readObject();
+                    return (UserData)in.readObject();
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return new FarnostData();
+        return new UserData();
     }
 
-    public static void saveFarnost(FarnostData data) {
+    public static void saveUser(UserData data) {
         try {
             Files.createDirectories(BIN_PATH.getParent());
             try (ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(BIN_PATH))) {
