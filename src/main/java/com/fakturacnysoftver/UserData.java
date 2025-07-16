13c98ff271fa2d10c218793854b61632b45bd4f9
Martin <martin.hronsky.007@gmail.com>
2025-07-16 17:59:06 +0200

    Rewriting values of user

--- src/main/java/com/fakturacnysoftver/UserData.java
+++ src/main/java/com/fakturacnysoftver/UserData.java
@@ -2,15 +2,15 @@ package com.fakturacnysoftver;
 
 import java.io.Serializable;
 
-public class FarnostData implements Serializable {
+public class UserData implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String nazov = "Rímskokatolícka cirkev, Farnosť Sedembolestnej Panny Márie";
-    private String sidlo = "M. Chrásteka 2772/28, 965 01 Žiar nad Hronom";
-    private String ico = "45021635";
-    private String dic = "2022704266";
-    private String kontakt = "mob. 0902 398 422, e‑mail: ziar.nhspm@fara.sk";
-    private String zastupeny = "JCLic. Vladimír Václavík, farár";
+    private String nazov = "Company, LLC";
+    private String sidlo = "City";
+    private String ico = "123123";
+    private String dic = "1231231231";
+    private String kontakt = "0900 012 345";
+    private String zastupeny = "Mgr. Prezident, Phd.";
 
     public String getNazov() {
         return this.nazov;
